9b7378764b47db0e80027e2583afbd9b42be520e
Clourc <corbino.lucas@gmail.com>
2023-08-28 09:34:32 +0200

    Vehicule CRUD

--- src/main/java/com/project/carfleet/controller/VehiculeController.java
+++ src/main/java/com/project/carfleet/controller/VehiculeController.java
@@ -6,22 +6,41 @@ import org.springframework.stereotype.Controller;
 
 import com.project.carfleet.repository.VehiculeRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class VehiculeController {
-    
-    @Autowired
-    private VehiculeRepository vehiculeRepository;
 
-    @GetMapping("/vehicules")
+    private final VehiculeRepository vehiculeRepository;
+    public VehiculeController(VehiculeRepository injectedRepository) {
+        this.vehiculeRepository = injectedRepository;
+    }
+
+    @GetMapping(value = "/vehicules")
     @ResponseBody
-    public List<Vehicule> getVehicules(){
+    public List<Vehicule> getVehicules(@RequestParam(defaultValue = "", required = false) String energy,
+                                       @RequestParam(defaultValue = "", required = false) String type){
+        boolean typeCheck = type.equals("citadine") || type.equals("utilitaire");
+        boolean energyCheck = energy.equals("essence") || energy.equals("électrique");
+        if((!type.isEmpty() && !typeCheck) || (!energy.isEmpty() && !energyCheck)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Incorrect parameter(s) provided");
+        }
+        if(!type.isEmpty()){
+            if(!energy.isEmpty()){
+                return vehiculeRepository.findVehiculeByTypeAndEnergy(type, energy);
+            }
+            return vehiculeRepository.findVehiculeByType(type);
+        } else {
+            if(!energy.isEmpty()){
+                return vehiculeRepository.findVehiculeByEnergy(energy);
+            }
+        }
         return vehiculeRepository.findAll();
     }
 
@@ -43,9 +62,13 @@ public class VehiculeController {
         return vehiculeToAdd;
     }
 
-    @GetMapping("vehicules/{id}/delete")
+    @DeleteMapping("vehicules/{id}/delete")
     public String deleteVehiculeById(@PathVariable Long id){
-        vehiculeRepository.deleteById(id);
-        return "Vehicule deleted";
+        Optional<Vehicule> optionalVehicule = vehiculeRepository.findById(id);
+        if(optionalVehicule.isPresent()){
+            vehiculeRepository.deleteById(id);
+            return "Vehicule id=" + id + " deleted";
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Vehicule not found");
     }
 }
--- src/main/java/com/project/carfleet/repository/VehiculeRepository.java
+++ src/main/java/com/project/carfleet/repository/VehiculeRepository.java
@@ -3,8 +3,21 @@ package com.project.carfleet.repository;
 import com.project.carfleet.entity.Vehicule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VehiculeRepository extends JpaRepository<Vehicule, Long>{
+
+    @Query(value = "SELECT * FROM vehicule WHERE energy = :energy", nativeQuery = true)
+    public List<Vehicule> findVehiculeByEnergy(@Param("energy") String energy);
+
+    @Query(value  = "SELECT * FROM vehicule WHERE type = :type", nativeQuery = true)
+    public List<Vehicule> findVehiculeByType(@Param("type") String type);
+
+    @Query(value = "SELECT * FROM vehicule WHERE type = :type AND energy = :energy", nativeQuery = true)
+    public List<Vehicule> findVehiculeByTypeAndEnergy(@Param("type") String type, @Param("energy") String energy);
 }
